b3ca4fdbc417b28c36ee1e0dba63aef3f553dc63
Cody <74194707+lewisoutdoorllc@users.noreply.github.com>
2021-11-23 16:51:19 -0500

    Update HeaderItem.js

--- componets/HeaderItem.js
+++ componets/HeaderItem.js
@@ -1,8 +1,8 @@
 function HeaderItem({ Icon, title }) {
     return (
         <div className="flex flex-col items-center cursor-pointer w-12 sm:w-20 hover:text-white group">
-            <Icon className="group-hover:animate-bounce h-8 mb-1 "/>
-            <p className=" tracking-widest opacity-0 group-hover:opacity-100 ">{title}</p>
+            <Icon className=" h-8 mb-1 "/>
+            <p className=" tracking-widest opacity-0 group-hover:opacity-100 animate-bounce ">{title}</p>
         </div>
     )
 }
