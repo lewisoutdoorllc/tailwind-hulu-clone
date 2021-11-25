9d3aad1172cbe8619d80697c8bcbb9dc79e2ee0f
Cody <74194707+lewisoutdoorllc@users.noreply.github.com>
2021-11-25 15:36:17 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   // mode: 'jit',
   purge: ['./pages/**/*.js', './components/**/*.js'],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
